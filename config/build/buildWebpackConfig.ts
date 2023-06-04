81277aa3622f0b8fb4c8fa66c1d76a5f381e6fd1
Andrew Krupenko <andrewkrupenko@Andrews-MacBook-Pro.local>
2023-06-05 00:39:17 +0300

    webpack-dev-server configuration

--- config/build/buildWebpackConfig.ts
+++ config/build/buildWebpackConfig.ts
@@ -4,11 +4,12 @@ import { buildPlugins } from "./buildPlugins";
 import { buildLoaders } from "./buildLoaders";
 import { buildResolvers } from "./buildResolvers";
 import { BuildOptions } from "./types/config";
+import { buildDevServer } from "./buildDevServer";
 
 export function buildWebpackConfig(
   options: BuildOptions
 ): webpack.Configuration {
-  const { paths, mode } = options;
+  const { paths, mode, isDev } = options;
   return {
     mode,
     entry: paths.entry,
@@ -23,5 +24,9 @@ export function buildWebpackConfig(
       rules: buildLoaders(),
     },
     resolve: buildResolvers(),
+    ...(isDev && {
+      devtool: "inline-source-map",
+      devServer: buildDevServer(options),
+    }),
   };
 }
