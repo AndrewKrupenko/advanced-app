cc9189ba017071e648ed82e26c052bf834b82f45
Andrew Krupenko <andrewkrupenko@Andrews-MacBook-Pro.local>
2023-06-26 00:46:15 +0300

    implemented routing with code splitting and lazy loading

--- config/build/buildDevServer.ts
+++ config/build/buildDevServer.ts
@@ -6,5 +6,6 @@ export function buildDevServer(options: BuildOptions): DevServerConfiguration {
   return {
     port: options.port,
     open: true, // automatically opens a browser window with our app
+    historyApiFallback: true, // without it page breaks after reloading it on the /about page
   };
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { Suspense } from "react";
+import { Link, Route, Routes } from "react-router-dom";
 
-import Counter from "./components/Counter";
 import "./index.scss";
+import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
+import { MainPageAsync } from "./pages/MainPage/MainPage.async";
 
 const App = () => {
   return (
     <div className="app">
-      Text here
-      <Counter />
+      <Link to={"/"}>Main page</Link>
+      <Link to={"/about"}>About</Link>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path={"/"} element={<MainPageAsync />} />
+          <Route path={"/about"} element={<AboutPageAsync />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 };
