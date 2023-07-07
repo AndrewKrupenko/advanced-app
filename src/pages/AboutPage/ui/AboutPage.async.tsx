cb80e4dbced310989fd3f33888af5241fd3e02d5
Andrew Krupenko <andrewkrupenko@Andrews-MacBook-Pro.local>
2023-07-08 00:17:01 +0300

    added Loader and NotFound page

--- src/pages/AboutPage/ui/AboutPage.async.tsx
+++ src/pages/AboutPage/ui/AboutPage.async.tsx
@@ -1,3 +1,12 @@
 import { lazy } from "react";
 
-export const AboutPageAsync = lazy(() => import("./AboutPage"));
+//export const AboutPageAsync = lazy(() => import("./AboutPage"));
+
+// Just Testing Loader, will delete in the future
+export const AboutPageAsync = lazy(
+  () =>
+    new Promise((resolve) => {
+      // @ts-ignore
+      setTimeout(() => resolve(import("./AboutPage")), 1500);
+    })
+);
--- src/pages/MainPage/ui/MainPage.async.tsx
+++ src/pages/MainPage/ui/MainPage.async.tsx
@@ -1,3 +1,12 @@
 import { lazy } from "react";
 
-export const MainPageAsync = lazy(() => import("./MainPage"));
+//export const MainPageAsync = lazy(() => import("./MainPage"));
+
+// Just Testing Loader, will delete in the future
+export const MainPageAsync = lazy(
+  () =>
+    new Promise((resolve) => {
+      // @ts-ignore
+      setTimeout(() => resolve(import("./MainPage")), 1500);
+    })
+);
