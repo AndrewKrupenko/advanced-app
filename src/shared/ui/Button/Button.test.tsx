c269e9ac29fff50a5d5aca792c4b1b854a82e753
Andrew Krupenko <andrewkrupenko@Andrews-MacBook-Pro.local>
2023-07-16 18:30:16 +0300

    react testing library

---
+++ src/shared/ui/Button/Button.test.tsx
@@ -0,0 +1,17 @@
+import { render, screen } from '@testing-library/react';
+
+import { Button, ThemeButton } from 'shared/ui/Button/Button';
+
+describe('Button', () => {
+  test('button is in the document', () => {
+    render(<Button>Test</Button>);
+    // to use toBeInTheDocument method set up testing-library/jest-dom
+    expect(screen.getByText('Test')).toBeInTheDocument();
+  });
+
+  test('button has clear class', () => {
+    render(<Button theme={ThemeButton.CLEAR}>Test</Button>);
+    expect(screen.getByText('Test')).toHaveClass('clear');
+    screen.debug();
+  });
+});
